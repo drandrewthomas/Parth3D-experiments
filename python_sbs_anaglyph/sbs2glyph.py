739391deb4ed5f880e97e1c191c12c3260fec1c2
Andrew Thomas <drandrewthomas@users.noreply.github.com>
2022-06-21 13:25:06 +0100

    Add files via upload

---
+++ python_sbs_anaglyph/sbs2glyph.py
@@ -0,0 +1,28 @@
+from PIL import Image
+
+#sbs = Image.open("beetle.jpg")
+#sbs = Image.open("cosfordplane.jpg")
+#sbs = Image.open("eglwysyberth.jpg")
+sbs = Image.open("druidscircle.jpg")
+#sbs = Image.open("bryncelliddu.jpg")
+#sbs = Image.open("uosapmm.jpg")
+sbsw, sbsh = sbs.size
+aglw = int(sbsw/2)
+
+agl = Image.new("RGBA",(aglw,sbsh),(255,255,255,255))
+sbspix = sbs.load()
+aglpix = agl.load()
+for y in range(0, sbsh):
+    for x in range(0, aglw):
+        left = sbspix[x, y]
+        right = sbspix[aglw + x, y]
+        aglpix[x, y] = (left[0], right[1], right[2])
+
+# Must save BEFORE displaying otherwise
+# image file will be zero bytes, at least on
+# Pydroid3
+agl.save("anaglyph.png")
+
+from matplotlib import pyplot as plt
+plt.imshow(agl)
+plt.show()
